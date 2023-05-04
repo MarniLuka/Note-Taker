4ba2548b4e29ffcabee41144f23c1923347026c2
Marni Luka <marnixluka@gmail.com>
2023-05-04 17:44:28 -0400

    started notes.js in routes folder

---
+++ routes/notes.js
@@ -0,0 +1,15 @@
+const notes = require('express').Router();
+const fs = require('fs');
+const uuid = require('../helpers/uuid');
+
+// GET Route for notes
+// localhost:3001/api/notes
+notes.get('/', (req, res) => {
+
+})
+
+notes.post('/', (req, res) => {
+
+})
+
+module.exports = notes
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Required packages
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 // Access files from routes
 const route = require('./routes/index')
 const PORT = process.env.PORT || 3001;
@@ -10,17 +9,17 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', route);
-
 app.use(express.static('public'));
+// Content coming from the routes folder will be accessed with /api in the URL
+app.use('/api', route);
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 // GET Route for notes
 app.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, './public/notes.html')))
+  res.sendFile(path.join(__dirname, '/public/notes.html')))
 
 
 app.listen(PORT, () => 
