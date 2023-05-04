640ccac3be1b0722b81cb9e758d64c2c84521e9b
Marni Luka <marnixluka@gmail.com>
2023-05-04 18:12:35 -0400

    added get and post to notes.js. Post need further debuggin

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+// Routes folder was created to help organize the different JS files so the server.js file isn't oversaturated with different information. 
+// There aren't many routes though so idk if this was actually necessary in this cse...
+
 const express = require('express');
 const notesRouter = require('./notes');
 const app = express();
--- routes/notes.js
+++ routes/notes.js
@@ -5,11 +5,41 @@ const uuid = require('../helpers/uuid');
 // GET Route for notes
 // localhost:3001/api/notes
 notes.get('/', (req, res) => {
+    console.info(`${req.method} request received to make a note`);
 
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+            res.json(JSON.parse(data));
+        }
+    })
 })
 
 notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to save a note`);
 
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'Success!',
+            body: newNote,
+        }
+
+        res.json(response);
+    } else {
+        res.json('Error in saving note')
+    }
 })
 
 module.exports = notes
